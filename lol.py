de83142e0b8f1955064499101052d075256f9b6a
James Kelly <jk377y@gmail.com>
2023-05-29 12:53:18 -0500

    updated readme notes

--- lol.py
+++ lol.py
@@ -1,4 +1,6 @@
+# import requests module to make HTTP requests
 import requests
+# import tabulate module to create tables in the terminal
 from tabulate import tabulate
 
 # ANSI escape sequences for color formatting
@@ -10,26 +12,27 @@ RESET = "\033[0m"
 # Make a GET request to the League of Legends API
 response = requests.get("http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/champion.json")
 
-# Extract the champion data from the API response
+# Extract the champion data from the API response and set it to a variable
 champion_data = response.json()
 champions = champion_data["data"]
 
 # Extract the champion IDs
 champion_ids = [champion["id"] for champion in champions.values()]
 
-# Define the number of columns for the table
+# Define the number of columns for the table to be created
 num_columns = 10
 
-# Chunk the champion IDs into groups based on the number of columns
+# Chunk the champion IDs into groups based on the number of columns, this creates the rows for the table
 champion_chunks = [champion_ids[i:i + num_columns] for i in range(0, len(champion_ids), num_columns)]
 
-# Create a table using the champion chunks
+# Create a table using the champion chunks as rows
 table = champion_chunks
 
-# Print the table in the terminal
+# Print the table in the terminal with tabulate module and grid format
 print(tabulate(table, tablefmt="grid"))
 
 while True:
+    # Prompt the user to enter the name of a champion based on the table
     user_input = input("Enter the name of a champion (q to quit): ")
 
     # Check if user wants to quit
@@ -49,11 +52,11 @@ while True:
         champion_id = chosen_champion["id"]
         champion_stats_url = f"http://ddragon.leagueoflegends.com/cdn/13.10.1/data/en_US/champion/{champion_id}.json"
 
-        # Make a GET request to retrieve the champion stats
+        # Make a GET request to retrieve the selected champion's stats
         stats_response = requests.get(champion_stats_url)
         stats_data = stats_response.json()
 
-        # Extract and display the desired stats
+        # Extract and display the desired stats (a little bit of destructuring was used, but probably not necessary at this point)
         info = stats_data["data"][champion_id]["info"]
         stats = stats_data["data"][champion_id]["stats"]
 
